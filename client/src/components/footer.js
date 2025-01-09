194b22979810e58fa0848e93e4f4779c4fa07ead
yash264 <yp20222068@gmail.com>
2025-01-09 23:39:04 +0530

    added backToTop button

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -6,11 +6,17 @@ import { SlLocationPin } from 'react-icons/sl';
 import { FaRegEnvelope } from 'react-icons/fa';
 import { FaTwitter } from "react-icons/fa";
 import enactusLogo from '../assets/enactusNavbarLogo.png';
+import { BsArrowUpCircleFill } from "react-icons/bs";
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
 
 function Footer() {
 
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Smooth scroll effect
+    });
+  };
 
   return (
     <footer className="relative bg-blueGray-200 pt-8 bg-black opacity-75 text-white">
@@ -43,7 +49,7 @@ function Footer() {
                 className="bg-white text-black shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
                 type="button"
               >
-                <FaInstagram className="fab fa-dribbble hover:text-4xl"  />
+                <FaInstagram className="fab fa-dribbble hover:text-4xl" />
               </button>
               <button
                 className="bg-white text-black shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2"
@@ -62,7 +68,7 @@ function Footer() {
 
                 <ul className="list-styled text-xs">
                   <li>
-                    <p class="flex items-center gap-2 justify-left m-2" > 
+                    <p class="flex items-center gap-2 justify-left m-2" >
                       <SlLocationPin className="mt-1 text-sm" />
                       Motilal Nehru National Institute of Technology
                       <br />
@@ -80,7 +86,7 @@ function Footer() {
                   <li>
                     <p class="flex items-center gap-2 justify-left m-2 " >
                       <CiPhone className="mt-1" />
-                    +91 8340556926</p>
+                      +91 8340556926</p>
                   </li>
                 </ul>
               </div>
@@ -89,9 +95,9 @@ function Footer() {
                 <img
                   src={enactusLogo}
                   alt="Enactus Logo"
-                  className="w-48 md:w-64 mx-auto"
+                  className="w-36 md:w-44 justify-left mx-auto"
                 />
-                <ul className="list-styled text-xs">
+                <ul className="list-styled text-xs p-2">
                   <li>
                     <p className="m-2" > <span style={{ color: "yellow" }} >En</span>trepreneurial - igniting business innovation with integrity and passion.</p>
                   </li>
@@ -107,20 +113,29 @@ function Footer() {
           </div>
         </div>
 
-        <hr className="my-6 border-blueGray-300" />
+        <hr />
 
-        <div className="flex flex-wrap items-center bg-black md:justify-between justify-center">
-          <div className="w-full md:w-4/12 mx-auto text-center">
+        <button className="scroll-to-top text-4xl text-red-600 hover:text-blue-400 bg-black-400 fixed"
+          style={{
+            bottom: "20px", /* Distance from the bottom of the viewport */
+            left: "20px", /* Distance from the right of the viewport */
+          }} 
+          onClick={scrollToTop} >
+          <BsArrowUpCircleFill />
+        </button>
 
-            <div className="text-sm text-blueGray-500 font-semibold py-1">
-              <span > © Enactus MNNIT-Allahabad 2k25 </span>
-            </div>
+      <div className="flex flex-wrap items-center bg-black md:justify-between justify-center">
+        <div className="w-full md:w-4/12 mx-auto text-center">
 
+          <div className="text-sm text-blueGray-500 font-semibold py-1">
+            <span > © Enactus MNNIT-Allahabad 2k25 </span>
           </div>
-        </div>
 
+        </div>
       </div>
-    </footer>
+
+    </div>
+    </footer >
   );
 }
 
