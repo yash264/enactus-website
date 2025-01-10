f442897ebaf72eed3cd6ccb3ea351b06cda56a7e
yash264 <yp20222068@gmail.com>
2025-01-10 12:06:28 +0530

    added background image

--- client/src/components/aboutus/aboutus.js
+++ client/src/components/aboutus/aboutus.js
@@ -3,30 +3,42 @@ import TextWrapper from './textWrapper';
 
 const AboutUsPage = () => {
 
-  // style={{ backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8)), url("https://clipground.com/images/enactus-logo-png-12.png")' }}
+  // https://www.enactusaustralia.org.au/wp-content/uploads/2023/08/SDG-Enactus-687x686-dcbade2.png
+
+  const backgroundStyle = {
+    backgroundImage: "url('https://www.enactusaustralia.org.au/wp-content/uploads/2023/08/SDG-Enactus-687x686-dcbade2.png')",
+    backgroundRepeat: "no-repeat", 
+    backgroundSize: "cover", 
+    backgroundPosition: "center", 
+    backgroundAttachment: "fixed",
+    //height: "100%", 
+    //width: "100%", 
+  };
 
   return (
-    <div className="relative">
-      <div className="text-center my-16 px-4">
-        <h2 className="text-5xl sm:text-6xl lg:text-7xl font-extrabold text-black tracking-tight mb-6">
+    <div className="relative" >
+      <div className="text-center my-6 px-4">
+        <h2 className="text-2xl sm:text-3xl lg:text-3xl font-bold text-black tracking-tight mb-2">
           Know About Us !!
         </h2>
-        <p className="text-lg md:text-2xl text-zinc-700 font-medium">
+        <p className="text-xl md:text-base text-zinc-700 italic">
           A little more about Enactus Allahabad
         </p>
       </div>
 
-      <div className=' bg-center bg-contain bg-no-repeat'  >
+      <div className=' bg-center bg-contain bg-no-repeat' style={backgroundStyle} >
         <div className="flex flex-col md:flex-row items-center justify-center ">
 
-          <div className="pl-8 p-4 md:w-1/2 flex justify-center "> {/* Remove mb-8 */}
-            <p className="text-sm leading-relaxed mt-8"> {/* Add mt-8 class */}
+          <div className="pl-8 p-4 md:w-1/2 flex justify-center " > {/* Remove mb-8 */}
+            <p className="text-sm leading-relaxed mt-8" > {/* Add mt-8 class */}
               <TextWrapper />
             </p>
           </div>
 
-          <div className="md:w-1/2 mt-4 md:mt-0 flex justify-center" >
-            <iframe className="elementor-video" frameborder="0" allowfullscreen="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" title="Enactus Impact" width="480" height="260" src="https://www.youtube.com/embed/1U3zF6ll27E?controls=1&amp;rel=0&amp;playsinline=0&amp;modestbranding=0&amp;autoplay=0&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fenactus.org&amp;widgetid=1" id="widget2" data-gtm-yt-inspected-6="true"></iframe>
+          <div className="md:w-1/2 pt-40 pb-40 md:mt-0 flex justify-center " >
+
+            <iframe className="elementor-video " frameborder="0" allowfullscreen="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" title="Enactus Impact" width="480" height="260" src="https://www.youtube.com/embed/1U3zF6ll27E?controls=1&amp;rel=0&amp;playsinline=0&amp;modestbranding=0&amp;autoplay=0&amp;enablejsapi=1&amp;origin=https%3A%2F%2Fenactus.org&amp;widgetid=1" id="widget2" data-gtm-yt-inspected-6="true" ></iframe>
+
           </div>
           
         </div>
