b371304c5523b254211cb36f3e1aa455ecfc44e7
atulpathak03 <atul3pathak@gmail.com>
2025-01-23 21:56:25 +0530

    product added

---
+++ client/src/images/asset.js
@@ -0,0 +1,20 @@
+import en1 from './en1.png'
+import en2 from './en2.png'
+import en3 from './en3.png'
+import en4 from './en4.png'
+import en5 from './en5.png'
+import en6 from './en6.png'
+import en7 from './en7.png'
+import en8 from './en8.png'
+
+export const asset = {
+    en1,
+    en2,
+    en3,
+    en4,
+    en5,
+    en6,
+    en7,
+    en8,
+
+}
